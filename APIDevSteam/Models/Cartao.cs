424946bcbf293d8e6f408b1449a0567a47aa5a90
Luiz G. <luiz.mwlo76@gmail.com>
2025-05-13 10:50:22 -0300

    Criação das relacões CartaoUsuario

---
+++ APIDevSteam/Models/Cartao.cs
@@ -0,0 +1,14 @@
+﻿namespace APIDevSteam.Models
+{
+    public class Cartao
+    {
+        public Guid CartaoId { get; set; }
+        public string NomeTitular { get; set; }
+        public int numeroCartao { get; set; }
+        public int codSeguranca { get; set; }
+        public DateOnly dataValidade { get; set; }
+
+        public ICollection<UsuarioCartao> UsuarioCartoes { get; set; } = new List<UsuarioCartao>();
+
+    }
+}
