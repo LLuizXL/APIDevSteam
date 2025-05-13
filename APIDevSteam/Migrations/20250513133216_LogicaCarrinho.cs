5978596a3a9f3807099f7876cca0e9d58c8d3821
Joaquim <jocaq124@gmail.com>
2025-05-13 10:32:52 -0300

    Feito Migration e Update Database

---
+++ APIDevSteam/Migrations/20250513133216_LogicaCarrinho.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIDevSteam.Migrations
+{
+    /// <inheritdoc />
+    public partial class LogicaCarrinho : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DataLancamento",
+                table: "Jogos",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataLancamento",
+                table: "Jogos");
+        }
+    }
+}
